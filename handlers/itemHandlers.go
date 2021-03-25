ecfdae92ee47ad5f771dbfffa9c51ccd61fd1e22
Kucheryavenko Alexey <kucheryavenko.alexey@gmail.com>
2021-03-25 13:25:13 +0300

    change 25-03-2021

--- handlers/itemHandlers.go
+++ handlers/itemHandlers.go
@@ -1,15 +1,19 @@
 package handlers
 
 import (
+	// "fmt"
 	"encoding/json"
 	"log"
 	"net/http"
-	//"strconv"
+	// "math"
 
 	"github.com/agk/specialist/c2/hw2/models"
 	//"github.com/gorilla/mux"
 )
 
+func initHeaders(writer http.ResponseWriter) {
+	writer.Header().Set("Content-Type", "application/json")
+}
 
 
 func Grab(writer http.ResponseWriter, request *http.Request) {
@@ -17,53 +21,31 @@ func Grab(writer http.ResponseWriter, request *http.Request) {
 	log.Println("grab data and creating new ....")
 	var Item models.Item
 	
-	
 	err := json.NewDecoder(request.Body).Decode(&Item)
 	if err != nil {
 		msg := models.Message{Message: "provideed json file is invalid"}
 		writer.WriteHeader(400)
-		json.NewEncoder(writer).Encode(msg)
+		err = json.NewEncoder(writer).Encode(msg)
+		if err != nil {
+			log.Fatal("can't encode message, something very wrong")
+		}
 		return
 	}
-	
-	Item.Nroots := 0
-
-	if Item.a == 0 {
-		if Item.b != 0 {
-			Item.Nroots = 1
-			// x = fraction(-c,b).toString()
-		} else if Item.c == 0 {
-			Item.Nroots = 1
-			// Все коэффициенты равны нулю; x - любое число.
-		} else {
-			Item.Nroots = 0
-		}
-	} else {
-		d = math.Pow(Item.b, 2) - 4*Item.a*Item.c
-		if d > 0 {
-			Item.Nroots = 2
-		} else if d == 0 {
-			Item.Nroots = 1
-		} else if d < 0 {
-			Item.Nroots = 0
-		}
-	}
-	
-
-	DB = append(DB, Item)
+	// fmt.Println(Item);
+	models.AddItem(Item)	//models.DB = append(models.DB, Item)
 	writer.WriteHeader(201)
-	msg := models.Message{Message: "Grabbing ok"}
-	json.NewEncoder(writer).Encode(msg)
+	// msg := models.Message{Message: "Grabbing ok"}
+	// json.NewEncoder(writer).Encode(msg)
+
+	
+	
+	
 	
 }
+
 func Solve(writer http.ResponseWriter, request *http.Request) {
 	initHeaders(writer)
-	if len(models.DB) > 0{
-		writer.WriteHeader(200)
-		json.NewEncoder(writer).Encode(Item)
-	}else{
-		writer.WriteHeader(404)
-		msg := models.Message{Message: "Error: data with that id not grab"}
-		json.NewEncoder(writer).Encode(msg)
-	} 
+	
+	json.NewEncoder(writer).Encode(models.GetItem())
+	writer.WriteHeader(200)
 }
--- models/data.go
+++ models/data.go
@@ -1,10 +1,43 @@
 package models
-
+import (
+	"log"
+	"math"
+)
 var DB []Item
 
 type Item struct {
-	a	int	`json:"a"`
-	b	int	`json:"b"`
-	c	int	`json:"c"`
-	Nroots	int	
+	A      int `json:"A"`
+	B      int `json:"B"`
+	C      int `json:"C"`
+	Nroots int `json:"Nroots"`
+}
+
+func AddItem(i Item) {
+	//i.Nroots := 0
+	if i.A == 0 {
+		if i.B != 0 {
+			i.Nroots = 1
+		} else if i.C == 0 {
+			i.Nroots = 1
+		} else {
+			i.Nroots = 0
+		}
+	} else {
+		d := int(math.Pow(float64(i.B), 2)) - 4*i.A*i.C
+		if d > 0 {
+			i.Nroots = 2
+		} else if d == 0 {
+			i.Nroots = 1
+		} else if d < 0 {
+			i.Nroots = 0
+		}
+	}
+	DB = append(DB, i)
+}
+
+func GetItem() Item {
+	if len(DB) == 0 {
+		log.Fatal("Error: data with that id not grab")
+	} 
+	return DB[len(DB)-1]
 }
